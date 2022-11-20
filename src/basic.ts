9a00fb47446c62bd21f4fa8e37a1fd18dace5dee
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-11-20 15:07:11 +0900

    Add greeting

--- src/basic.ts
+++ src/basic.ts
@@ -8,3 +8,9 @@ const user = {
 };
 //user.location;
 console.log("Hello world!");
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Brendan", new Date());
